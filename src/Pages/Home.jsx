13c88af6991b731dd0a708d2c6cdafe71f6ba7c0
Dhart1984 <donalde.hart@gmail.com>
2024-02-22 11:46:49 -0500

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from "@material-tailwind/react"
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        This is my home page.
+    
+      </h1>
+      <Button>my new button</Button>
+    </div>
+  )
+}
